77f5110ffe4756c3c3f05e8f1da3986a59d89b5f
Chris.V <cvasqu09@gmail.com>
2018-02-23 00:36:12 -0600

    Added few basic routes for events and users
    Added jquery for bootstrap.
    Added ESLint with some initial rules.
    Modified some of the return codes and constraints on event schema
    Added patch to REST API
    Added info for event model and patch to api
    Added the initial routes and models for the user
    Added the get, post, patch, delete for the user
    Redirect invalid routes to main page.
    Added dotenv for reading environment variables.

---
+++ server/models/event.js
@@ -0,0 +1,32 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var categoryValidator = function (categoryArray) {
+  console.log('Category is: ' + categoryArray);
+  console.log(categoryArray.length);
+  if (categoryArray == null || categoryArray.length == 0) {
+    return false;
+  }
+  return true;
+};
+
+var eventSchema = new Schema({
+  eventName: { type: String, required: true },
+  categories: {
+    type: Array,
+    required: true,
+    validate: {
+      validator: categoryValidator,
+      message: 'Categories cannot be empty.'
+    }
+  },
+  numPeople: { type: Number, required: true }, // Do we want ranges? Querying may be better if we have just a number
+  location: { type: String }, // required: true} How are we going to store the location?
+  startTime: { type: Number }, // How are we going to store the time and what time zone will we use as reference?
+  endTime: { type: Number },
+  description: { type: String }, // Limit the length?
+  eventOwner: { type: String, required: true }
+},
+{ versionKey: false });
+
+module.exports = mongoose.model('Event', eventSchema);
