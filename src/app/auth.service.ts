95b66d00b617dbfd44cba6ae6509000990fe3341
JStuve <stuve.josh@gmail.com>
2018-02-28 15:19:40 -0600

    Logins now save, logout and check for authentication

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -19,9 +19,35 @@ export class AuthService {
     this.auth0.authorize();
   }
 
+  public logout(): void {
+
+    localStorage.removeItem('access_token');
+    localStorage.removeItem('id_token');
+    localStorage.removeItem('expires_at');
+    console.log("Logout Successful!");
+  }
+
   public handleAuthentication(): void {
     this.auth0.parseHash((err, authResult) => {
-      console.log(authResult);
+      if (authResult && authResult.accessToken && authResult.idToken) {
+        window.location.hash = ''; //Without this, the URL will contain token information
+        this.setSession(authResult);
+        console.log("Login Successful!");
+      } else if (err) {
+        console.log("Login Failed...");
+      }
     });
+  }
+
+  private setSession(authResult): void {
+    const expiresAt = JSON.stringify((authResult.expiresIn * 1000) + new Date().getTime());
+    localStorage.setItem('access_token', authResult.accessToken);
+    localStorage.setItem('id_token', authResult.idToken);
+    localStorage.setItem('expires_at', expiresAt);
+  }
+
+  public isAuthenticated(): boolean {
+    const expiresAt = JSON.parse(localStorage.getItem('expires_at'));
+    return new Date().getTime() < expiresAt;
   }
 }
