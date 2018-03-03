79444f177b78db68f21f1c977f9fdebf25a13ca9
Chris.V <cvasqu09@gmail.com>
2018-03-03 15:45:24 -0600

    Removed component tests checking for old content for now

--- src/app/ice-number/ice-number.component.spec.ts
+++
@@ -1,7 +0,0 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { IceNumberComponent } from './ice-number.component';
-
-describe('IceNumberComponent', () => {
-
-});
