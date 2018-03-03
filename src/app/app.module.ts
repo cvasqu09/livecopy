4300cbe6c2ef50f51938a4e8845102913169bfe6
Chris.V <cvasqu09@gmail.com>
2018-03-03 01:29:03 -0600

    Added components for the home screen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HttpModule } from '@angular/http';
 import { UserService } from './user/user.service';
-
+import { HomeComponent } from './home/home.component';
+import { SearchComponent } from './home/search/search.component';
+import { GoogleMapComponent } from './home/google-map/google-map.component';
+import { SearchBarComponent } from './home/search/search-bar/search-bar.component';
+import { DropdownComponent } from './home/search/dropdown/dropdown.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SearchComponent,
+    GoogleMapComponent,
+    SearchBarComponent,
+    DropdownComponent
   ],
   imports: [
     BrowserModule,
