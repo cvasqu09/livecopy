b8d1319391e1b2607a153d3b8b6fa9834eb5e0bf
Chris.V <cvasqu09@gmail.com>
2018-03-16 19:47:18 -0600

    Updated packages. Made the server reachable at port 3000
    Port must be 3000 for tests. If this tries using the process.env.PORT then the wrong endpoint is being hit because ng test runs on port 9xxx

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.all('*', function (req, res) {
 });
 
 // Set Port
-const port = process.env.PORT || '3000';
+const port = '3000';
 app.set('port', port);
 const server = http.createServer(app);
 
